b4ba096f49311a8a51536bd2cb3e322b4271ae29
kisssick0 <100334382+kisssick0@users.noreply.github.com>
2023-10-10 15:53:16 +0300

    правки дз3

--- 03/test_custom_list.py
+++ 03/test_custom_list.py
@@ -11,22 +11,62 @@ class TestCustomList(unittest.TestCase):
         cl2 = custom_list.CustomList([4, 5])
         sum_cl1_cl2 = [5, 7, 3]
 
-        self.assertEqual(cl1 + cl2, sum_cl1_cl2)
-        self.assertEqual(cl2 + cl1, sum_cl1_cl2)
+        self.assertEqual(list(cl1 + cl2), sum_cl1_cl2)
+        self.assertEqual(list(cl2 + cl1), sum_cl1_cl2)
 
-        self.assertEqual(cl1, custom_list.CustomList([1, 2, 3]))
-        self.assertEqual(cl2, custom_list.CustomList([4, 5]))
+        self.assertEqual(list(cl1), list(custom_list.CustomList([1, 2, 3])))
+        self.assertEqual(list(cl2), list(custom_list.CustomList([4, 5])))
+
+        cl1 = custom_list.CustomList([4, 5])
+        cl2 = custom_list.CustomList([1, 2, 3])
+        sum_cl1_cl2 = [5, 7, 3]
+
+        self.assertEqual(list(cl1 + cl2), sum_cl1_cl2)
+        self.assertEqual(list(cl2 + cl1), sum_cl1_cl2)
+
+        self.assertEqual(list(cl1), list(custom_list.CustomList([4, 5])))
+        self.assertEqual(list(cl2), list(custom_list.CustomList([1, 2, 3])))
+
+        cl1 = custom_list.CustomList([4, 5, 6])
+        cl2 = custom_list.CustomList([1, 2, 3])
+        sum_cl1_cl2 = [5, 7, 9]
+
+        self.assertEqual(list(cl1 + cl2), sum_cl1_cl2)
+        self.assertEqual(list(cl2 + cl1), sum_cl1_cl2)
+
+        self.assertEqual(list(cl1), list(custom_list.CustomList([4, 5, 6])))
+        self.assertEqual(list(cl2), list(custom_list.CustomList([1, 2, 3])))
 
     def test_sum_custom_list_and_list(self):
         clst = custom_list.CustomList([1, 2, 3])
         lst = [4, 5]
         sum_cl_lst = [5, 7, 3]
 
-        self.assertEqual(clst + lst, sum_cl_lst)
-        self.assertEqual(lst + clst, sum_cl_lst)
+        self.assertEqual(list(clst + lst), sum_cl_lst)
+        self.assertEqual(list(lst + clst), sum_cl_lst)
 
-        self.assertEqual(clst, custom_list.CustomList([1, 2, 3]))
+        self.assertEqual(list(clst), list(custom_list.CustomList([1, 2, 3])))
         self.assertEqual(lst, [4, 5])
+
+        clst = custom_list.CustomList([4, 5])
+        lst = [1, 2, 3]
+        sum_cl_lst = [5, 7, 3]
+
+        self.assertEqual(list(clst + lst), sum_cl_lst)
+        self.assertEqual(list(lst + clst), sum_cl_lst)
+
+        self.assertEqual(list(clst), list(custom_list.CustomList([4, 5])))
+        self.assertEqual(lst, [1, 2, 3])
+
+        clst = custom_list.CustomList([4, 5, 6])
+        lst = [1, 2, 3]
+        sum_cl_lst = [5, 7, 9]
+
+        self.assertEqual(list(clst + lst), sum_cl_lst)
+        self.assertEqual((lst + clst), sum_cl_lst)
+
+        self.assertEqual(list(clst), list(custom_list.CustomList([4, 5, 6])))
+        self.assertEqual(lst, [1, 2, 3])
 
     def test_sub_two_custom_lists(self):
         cl1 = custom_list.CustomList([1, 2, 3])
@@ -34,11 +74,33 @@ class TestCustomList(unittest.TestCase):
         sub_cl1_cl2 = [-3, -3, 3]
         sub_cl2_cl1 = [3, 3, -3]
 
-        self.assertEqual(cl1 - cl2, sub_cl1_cl2)
-        self.assertEqual(cl2 - cl1, sub_cl2_cl1)
+        self.assertEqual(list(cl1 - cl2), sub_cl1_cl2)
+        self.assertEqual(list(cl2 - cl1), sub_cl2_cl1)
 
-        self.assertEqual(cl1, custom_list.CustomList([1, 2, 3]))
-        self.assertEqual(cl2, custom_list.CustomList([4, 5]))
+        self.assertEqual(list(cl1), list(custom_list.CustomList([1, 2, 3])))
+        self.assertEqual(list(cl2), list(custom_list.CustomList([4, 5])))
+
+        cl1 = custom_list.CustomList([4, 5])
+        cl2 = custom_list.CustomList([1, 2, 3])
+        sub_cl1_cl2 = [3, 3, -3]
+        sub_cl2_cl1 = [-3, -3, 3]
+
+        self.assertEqual(list(cl1 - cl2), sub_cl1_cl2)
+        self.assertEqual(list(cl2 - cl1), sub_cl2_cl1)
+
+        self.assertEqual(list(cl1), list(custom_list.CustomList([4, 5])))
+        self.assertEqual(list(cl2), list(custom_list.CustomList([1, 2, 3])))
+
+        cl1 = custom_list.CustomList([4, 5, 6])
+        cl2 = custom_list.CustomList([1, 2, 3])
+        sub_cl1_cl2 = [3, 3, 3]
+        sub_cl2_cl1 = [-3, -3, -3]
+
+        self.assertEqual(list(cl1 - cl2), sub_cl1_cl2)
+        self.assertEqual(list(cl2 - cl1), sub_cl2_cl1)
+
+        self.assertEqual(list(cl1), list(custom_list.CustomList([4, 5, 6])))
+        self.assertEqual(list(cl2), list(custom_list.CustomList([1, 2, 3])))
 
     def test_sub_custom_list_and_list(self):
         clst = custom_list.CustomList([1, 2, 3])
@@ -46,11 +108,33 @@ class TestCustomList(unittest.TestCase):
         sub_cl_lst = [-3, -3, 3]
         sub_lst_cl = [3, 3, -3]
 
-        self.assertEqual(clst - lst, sub_cl_lst)
-        self.assertEqual(lst - clst, sub_lst_cl)
+        self.assertEqual(list(clst - lst), sub_cl_lst)
+        self.assertEqual(list(lst - clst), sub_lst_cl)
 
-        self.assertEqual(clst, custom_list.CustomList([1, 2, 3]))
+        self.assertEqual(list(clst), list(custom_list.CustomList([1, 2, 3])))
         self.assertEqual(lst, [4, 5])
+
+        clst = custom_list.CustomList([4, 5])
+        lst = custom_list.CustomList([1, 2, 3])
+        sub_cl_lst = [3, 3, -3]
+        sub_lst_cl = [-3, -3, 3]
+
+        self.assertEqual(list(clst - lst), sub_cl_lst)
+        self.assertEqual(list(lst - clst), sub_lst_cl)
+
+        self.assertEqual(list(clst), list(custom_list.CustomList([4, 5])))
+        self.assertEqual(lst, [1, 2, 3])
+
+        clst = custom_list.CustomList([1, 2, 3])
+        lst = custom_list.CustomList([4, 5, 6])
+        sub_cl_lst = [-3, -3, -3]
+        sub_lst_cl = [3, 3, 3]
+
+        self.assertEqual(list(clst - lst), sub_cl_lst)
+        self.assertEqual(list(lst - clst), sub_lst_cl)
+
+        self.assertEqual(list(clst), list(custom_list.CustomList([1, 2, 3])))
+        self.assertEqual(lst, [4, 5, 6])
 
     def test_lt(self):
         cl1 = custom_list.CustomList([1, 2, 3])
@@ -172,11 +256,17 @@ class TestCustomList(unittest.TestCase):
         self.assertEqual(cl1, custom_list.CustomList([1, 2, 3]))
         self.assertEqual(cl2, custom_list.CustomList([1, 0, 5]))
 
-    def test_str(self):
+    def test_print_str(self):
         clst = custom_list.CustomList([1, 2, 3])
         with patch('sys.stdout', new=StringIO()) as fake_out:
             print(clst)
             self.assertEqual(fake_out.getvalue(), "[1, 2, 3] sum=6\n")
+
+        self.assertEqual(clst, custom_list.CustomList([1, 2, 3]))
+
+    def test_str(self):
+        clst = custom_list.CustomList([1, 2, 3])
+        self.assertEqual(str(clst), "[1, 2, 3] sum=6")
 
         self.assertEqual(clst, custom_list.CustomList([1, 2, 3]))
 
