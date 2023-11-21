4b0fc1c96645c0338f4ba1a14060284df14ac201
kisssick0 <100334382+kisssick0@users.noreply.github.com>
2023-11-21 09:20:10 +0300

    правки ДЗ5

--- 05/test_lru_cache.py
+++ 05/test_lru_cache.py
@@ -3,16 +3,19 @@ from lru_cache import LRUCache
 
 
 class TestLRUCache(unittest.TestCase):
-    def setUp(self):
+    def test_set_get(self):
         self.cache = LRUCache(2)
         self.cache.set("k1", "val1")
         self.cache.set("k2", "val2")
 
-    def test_set_get(self):
         self.assertEqual(self.cache.get("k1"), "val1")
         self.assertEqual(self.cache.get("k2"), "val2")
 
     def test_update_value(self):
+        self.cache = LRUCache(3)
+        self.cache.set("k1", "val1")
+        self.cache.set("k2", "val2")
+
         self.assertEqual(self.cache.get("k1"), "val1")
         self.assertEqual(self.cache.get("k2"), "val2")
 
@@ -25,21 +28,40 @@ class TestLRUCache(unittest.TestCase):
         self.assertEqual(self.cache.get("k1"), "val1111")
         self.assertEqual(self.cache.get("k2"), "val222")
 
-
     def test_missing_key(self):
+        self.cache = LRUCache(2)
+        self.cache.set("k1", "val1")
+        self.cache.set("k2", "val2")
+
         self.assertIsNone(self.cache.get("k3"))
+        self.assertEqual(self.cache.get("k2"), "val2")
+        self.assertEqual(self.cache.get("k1"), "val1")
 
         self.cache.set("k3", "val3")
+
         self.assertEqual(self.cache.get("k3"), "val3")
+        self.assertIsNone(self.cache.get("k2"))
+        self.assertEqual(self.cache.get("k1"), "val1")
 
     def test_reaching_the_limit(self):
+        self.cache = LRUCache(2)
+        self.cache.set("k1", "val1")
+        self.cache.set("k2", "val2")
         self.assertEqual(self.cache.get("k1"), "val1")
         self.assertEqual(self.cache.get("k2"), "val2")
 
         self.cache.set("k3", "val3")
         self.assertIsNone(self.cache.get("k1"))
-
         self.assertEqual(self.cache.get("k2"), "val2")
+        self.assertEqual(self.cache.get("k3"), "val3")
+
+    def test_limit_1(self):
+        self.cache = LRUCache(1)
+        self.cache.set("k1", "val1")
+        self.assertEqual(self.cache.get("k1"), "val1")
+        self.cache.set("k2", "val2")
+        self.assertEqual(self.cache.get("k2"), "val2")
+        self.assertIsNone(self.cache.get("k1"))
 
 
 if __name__ == '__main__':
